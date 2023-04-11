cc7afab4a4fddf6526d3dbf690d1d272c7903796
unknown <Administrateur@local.edimbourg.cub.sioplc.fr>
2023-04-11 09:28:03 +0200

    2eme commit

---
+++ exerciceAUDIT2services.ps1
@@ -0,0 +1,21 @@
+﻿# Définition des variables
+$currentTime = Get-Date
+$outputFile = "C:\auditDHCP.txt"
+$date = Get-Date -Format "yyyy-MM-dd"
+$fichier = "C:\auditDHCP.txt"
+
+# Récupération des plages DHCP présentes sur le serveur
+$plages = Get-DhcpServerv4Scope
+
+# Ecriture dans le fichier et affichage à l'écran
+foreach ($plage in $plages) {
+    $nom = $plage.Name
+    $statut = $plage.State
+    if ($statut -eq "Active") {
+        Add-Content $fichier "$date - $nom : $statut"
+        Write-Host "$date - $nom : $statut"
+    } else {
+        Add-Content $fichier "$(Write-Host $date - $nom : $statut -ForegroundColor Red)"
+        Write-Host $date - $nom : $statut -ForegroundColor Red
+    }
+}
