dcc738b4933380333d12dd3c9c23d9bf0372eb07
unknown <Administrateur@local.edimbourg.cub.sioplc.fr>
2023-04-11 09:14:03 +0200

    premier commit

---
+++ exerciceAUDITservices.ps1
@@ -0,0 +1,12 @@
+﻿$currentTime = Get-Date
+$outputFile = "C:\auditServices.txt"
+
+$dhcpService = Get-Service -Name dhcp
+$dnsService = Get-Service -Name dns
+$adService = Get-Service -Name adws
+
+if (($dhcpService.Status -eq "Running") -and ($dnsService.Status -eq "Running") -and ($adService.Status -eq "Running")) {
+    Add-Content -Path $outputFile -Value "Les services DHCP, DNS et AD sont en cours d'exécution à $currentTime"
+} else {
+    Add-Content -Path $outputFile -Value "Au moins l'un des services DHCP, DNS ou AD n'est pas en cours d'exécution à $currentTime"
+}
